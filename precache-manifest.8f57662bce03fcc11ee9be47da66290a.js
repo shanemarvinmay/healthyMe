cc1c8f625b0e9518813adb321c44764cff502de2
shanemarvinmay <shanemarvinmay@gmail.com>
2020-03-15 20:45:40 -0500

    Updates

--- precache-manifest.8f57662bce03fcc11ee9be47da66290a.js
+++ precache-manifest.8f57662bce03fcc11ee9be47da66290a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3d053283974164f3fb00ef944bdff3c1",
+    "revision": "e402342a1fff9d948a452f59e80ab774",
     "url": "/healthyMe/index.html"
   },
   {
-    "revision": "c740eb2fc5c932934605",
+    "revision": "ecc30d8e666d9c2709b8",
     "url": "/healthyMe/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "ba25e513ae9c7e7f723a",
-    "url": "/healthyMe/static/js/2.88942a30.chunk.js"
+    "revision": "be60b44834cf06612119",
+    "url": "/healthyMe/static/js/2.9bf8844b.chunk.js"
   },
   {
     "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
-    "url": "/healthyMe/static/js/2.88942a30.chunk.js.LICENSE.txt"
+    "url": "/healthyMe/static/js/2.9bf8844b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c740eb2fc5c932934605",
-    "url": "/healthyMe/static/js/main.13870fc9.chunk.js"
+    "revision": "ecc30d8e666d9c2709b8",
+    "url": "/healthyMe/static/js/main.bab599d6.chunk.js"
   },
   {
     "revision": "2917585be5d5e4836e0a",
