98f2931f606d98ae7f73281ff51a28edd1961c58
shanemarvinmay <shanemarvinmay@gmail.com>
2020-03-15 19:33:39 -0500

    react setup done

---
+++ src/components/Auth.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+let Auth = (props) => {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+        <div>
+            <h1>Login</h1>
+            <input />
+            <input />
+            <button>login</button>
+        </div>
+      {/* <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button> */}
+    </div>
+  );
+}
+
+export default Auth;
